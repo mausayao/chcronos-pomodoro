ac51e88f5130d891d9d9425f3f52fc29ec660e8f
mausayao <mauricio.sayao@gmail.com>
2025-12-07 11:07:18 -0400

    organize folder and files

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import './styles/theme.css';
 import './styles/global.css';
 import { Container } from './components/Container';
 
+
 export function App() {
     return (
         <>
-       <Container>
-        <section>Logo</section>
-       </Container>
-       <Container>
-        <section>Menu</section>
-       </Container>
+        <Container>
+            <section>Logo</section>
+        </Container>
+        <Container>
+            <section>Menu</section>
+        </Container>
         </>
     );
 }
