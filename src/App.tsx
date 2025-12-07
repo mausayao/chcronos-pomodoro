9f5e69197004fd5ece795dcb79426b489610fbe6
mausayao <mauricio.sayao@gmail.com>
2025-12-07 14:14:10 -0400

    menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import './styles/theme.css';
-import './styles/global.css';
+import "./styles/theme.css";
+import "./styles/global.css";
 
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 export function App() {
-    return (
-        <>
-        <Container>
-           <Logo/>
-        </Container>
-        <Container>
-            <section>Menu</section>
-        </Container>
-        </>
-    );
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Menu />
+      </Container>
+    </>
+  );
 }
