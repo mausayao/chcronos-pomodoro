0fefd53c19ef25c373a15e957b0ae6e73570b49f
mausayao <mauricio.sayao@gmail.com>
2025-12-11 18:19:52 -0400

    template component

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,12 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 
-import { Container } from "./components/Container";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
-import { CountDown } from "./components/CountDown";
-import { Input } from "./components/Input";
-import { Cycles } from "./components/Cycles";
-import { Button } from "./components/Button";
-import { PlayCircleIcon } from "lucide-react";
-import { Footer } from "./components/Footer";
+import { Home } from "./pages/home";
 
 export function App() {
   return (
     <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className="form" action="">
-          <div className="formRow">
-            <Input id="formId" labelText="task" type="text" />
-          </div>
-
-          <div className="formRow">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className="formRow">
-            <Cycles />
-          </div>
-
-          <div className="formRow">
-            <Button icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
+      <Home />
     </>
   );
 }
