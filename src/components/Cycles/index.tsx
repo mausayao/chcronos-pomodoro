521360e5920f5fa98c453b7d9a175ec16c6474ae
mausayao <mauricio.sayao@gmail.com>
2025-12-21 11:09:14 -0400

    cycle logic

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,18 +1,36 @@
+import { useTaskContext } from "../../contexts/TaskContext/UseTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { nextCycleType } from "../../utils/getNextCycleType";
 import styles from "./styles.module.css";
 
 export function Cycles() {
+  const { state } = useTaskContext();
+
+  const cycles = Array.from({ length: state.currentCycle });
+
+  const cycleTypeStep = {
+    workTime: "foco",
+    shortBreakTime: "descanso curto",
+    longBreakTime: "descanso longo",
+  };
+
   return (
     <div className={styles.cycles}>
       <span>Ciclos:</span>
       <div className={styles.cyclesDots}>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.longBreakTime}`}></span>
+        {cycles.map((_, index) => {
+          const nextCycle = getNextCycle(index);
+          const cycleType = nextCycleType(nextCycle);
+
+          return (
+            <span
+              key={nextCycle}
+              className={`${styles.cycleDot} ${styles[cycleType]}`}
+              aria-label={`Indicador de cilco de ${cycleTypeStep[cycleType]}`}
+              title={`Indicador de cilco de ${cycleTypeStep[cycleType]}`}
+            ></span>
+          );
+        })}
       </div>
     </div>
   );
