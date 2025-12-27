81599f0b31d75abb22370d067890376d1262ae75
mausayao <mauricio.sayao@gmail.com>
2025-12-27 16:03:47 -0400

    play sound

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useRef } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./TaskReducer";
 import { TimeWorkerManager } from "../../worker/TimeWorkerManager";
 import { TaskActionTypes } from "./TaskAction";
+import { loadSound } from "../../utils/loadSound";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -12,20 +13,7 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
   const worker = TimeWorkerManager.getInstance();
-  worker.onmessage((e) => {
-    
-    const countDownSeconds = e.data;
-
-    if (countDownSeconds <= 0) {
-      dispatch({ type: TaskActionTypes.COMPLETED_TASK });
-      worker.terminate();
-    } else {
-      dispatch({
-        type: TaskActionTypes.COUNT_DOWN,
-        payload: { secondsRemaining: countDownSeconds },
-      });
-    }
-  });
+  const loadSoundEffect = useRef<ReturnType<typeof loadSound> | null>(null);
 
   useEffect(() => {
     if (!state.activeTask) {
@@ -33,6 +21,32 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     }
     worker.postMessage(state);
   }, [worker, state]);
+
+  useEffect(() => {
+    worker.onmessage((e) => {
+      const countDownSeconds = e.data;
+
+      if (countDownSeconds <= 0) {
+        if (loadSoundEffect.current) {
+          loadSoundEffect.current();
+          loadSoundEffect.current = null;
+        }
+        dispatch({ type: TaskActionTypes.COMPLETED_TASK });
+        worker.terminate();
+      } else {
+        dispatch({
+          type: TaskActionTypes.COUNT_DOWN,
+          payload: { secondsRemaining: countDownSeconds },
+        });
+      }
+    });
+
+    if (state.activeTask && loadSoundEffect.current === null) {
+      loadSoundEffect.current = loadSound();
+    } else {
+      loadSoundEffect.current = null;
+    }
+  }, [state.activeTask, worker]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
