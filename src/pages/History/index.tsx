2af6ff439abef5b8f97582f093655dce623d964d
mausayao <mauricio.sayao@gmail.com>
2025-12-28 14:49:56 -0400

    clear history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -9,10 +9,11 @@ import { useTaskContext } from "../../contexts/TaskContext/UseTaskContext";
 import { formatDate } from "../../utils/formatDate";
 import { getTaskStatus } from "../../utils/getTaskStatus";
 import { sortTask, type SortTaskOptions } from "../../utils/sortTask";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function History() {
-  const { state } = useTaskContext();
+  const { state, dispatch } = useTaskContext();
+  const hasTasks = state.tasks.length > 0;
 
   const [sortState, setSortState] = useState<SortTaskOptions>(() => {
     return {
@@ -39,69 +40,83 @@ export function History() {
       direction: newDirection,
     });
   }
+  function clearHistory() {
+    if (!confirm("Deseja deletar o Histórico")) return;
+    dispatch({ type: "RESET_STATE" });
+  }
 
   return (
     <MainTemplate>
       <Container>
         <Heading>
           <span>Histórico</span>
-          <span className={styles.buttonContainer}>
-            <Button
-              icon={<TrashIcon />}
-              color="red"
-              aria-label="Apagar histórico"
-              title="Apagar histórico"
-            />
-          </span>
+          {hasTasks && (
+            <span className={styles.buttonContainer}>
+              <Button
+                icon={<TrashIcon />}
+                color="red"
+                aria-label="Apagar histórico"
+                title="Apagar histórico"
+                onClick={clearHistory}
+              />
+            </span>
+          )}
         </Heading>
       </Container>
       <Container>
-        <div className={styles.responsiveTable}>
-          <table>
-            <thead>
-              <tr>
-                <th
-                  onClick={() => handleSortTask({ field: "name" })}
-                  className={styles.thSort}
-                >
-                  Tarefa
-                </th>
-                <th
-                  onClick={() => handleSortTask({ field: "duration" })}
-                  className={styles.thSort}
-                >
-                  Dureção
-                </th>
-                <th
-                  onClick={() => handleSortTask({ field: "startDate" })}
-                  className={styles.thSort}
-                >
-                  Data
-                </th>
-                <th>Status</th>
-                <th>Tipo</th>
-              </tr>
-            </thead>
-            <tbody>
-              {sortState.tasks.map((task) => {
-                const taskTypeEnum = {
-                  workTime: "Foco",
-                  shortBreakTime: "Descanso curto",
-                  longBreakTime: "Descanso Longo",
-                };
-                return (
-                  <tr key={task.id}>
-                    <td>{task.name}</td>
-                    <td>{task.duration} min</td>
-                    <td>{formatDate(task.startDate)}</td>
-                    <td>{getTaskStatus(task, state.activeTask)}</td>
-                    <td>{taskTypeEnum[task.type]}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+        {hasTasks && (
+          <div className={styles.responsiveTable}>
+            <table>
+              <thead>
+                <tr>
+                  <th
+                    onClick={() => handleSortTask({ field: "name" })}
+                    className={styles.thSort}
+                  >
+                    Tarefa
+                  </th>
+                  <th
+                    onClick={() => handleSortTask({ field: "duration" })}
+                    className={styles.thSort}
+                  >
+                    Dureção
+                  </th>
+                  <th
+                    onClick={() => handleSortTask({ field: "startDate" })}
+                    className={styles.thSort}
+                  >
+                    Data
+                  </th>
+                  <th>Status</th>
+                  <th>Tipo</th>
+                </tr>
+              </thead>
+              <tbody>
+                {sortState.tasks.map((task) => {
+                  const taskTypeEnum = {
+                    workTime: "Foco",
+                    shortBreakTime: "Descanso curto",
+                    longBreakTime: "Descanso Longo",
+                  };
+                  return (
+                    <tr key={task.id}>
+                      <td>{task.name}</td>
+                      <td>{task.duration} min</td>
+                      <td>{formatDate(task.startDate)}</td>
+                      <td>{getTaskStatus(task, state.activeTask)}</td>
+                      <td>{taskTypeEnum[task.type]}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        )}
+        {!hasTasks && (
+          <p style={{ textAlign: "center", fontWeight: "bold" }}>
+            Não possui tarefas criadas!
+          </p>
+        )}
       </Container>
     </MainTemplate>
   );
